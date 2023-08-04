1d3b02ad95a714bf1b66e98649339f045966c88f
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-08-04 13:27:22 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
-import io
-import os
-import textwrap
-import numpy as np
+from flask import Flask, request, jsonify
 import pandas as pd
 import openai
-from flask import Flask, jsonify
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+import os
+import textwrap
+import io
 from azure.storage.blob import BlobServiceClient
+import numpy as np
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -25,49 +25,56 @@ secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 # Get the OpenAI API key from the Key Vault
 openai.api_key = secret_client.get_secret("openai-api-key").value
 
-def analyze_text(text):
-    """Performs sentiment analysis using GPT-3."""
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system", "content": "You are a research assistant analyzing public sentiment towards the Panama Papers tax scandal. Analyze the following text for key themes, sentiment, and emotional tone:"},
-            {"role": "user", "content": text}
-        ],
-        temperature=0.3,
-        max_tokens=6000
-    )
-    return response['choices'][0]['message']['content'].strip()
+def chunker(seq, size):
+    """Splits the data into chunks."""
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+def analyze_text_in_chunks(df, chunk_size=10):
+    """Performs topic modeling, sentiment analysis, and emotional tone analysis using GPT-3 on chunks of data."""
+    df['Analysis'] = np.nan
+    for i, chunk in enumerate(chunker(df, chunk_size)):
+        text = ' '.join(chunk['text'])
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=[
+                {"role": "system", "content": "You are a research assistant analyzing public sentiment towards the Panama Papers tax scandal. Analyze the following text for key themes, sentiment, and emotional tone:"},
+                {"role": "user", "content": text}
+            ],
+            temperature=0.3,
+            max_tokens=16000
+        )
+        df.loc[chunk.index, 'Analysis'] = response['choices'][0]['message']['content'].strip()
+    return df
 
 @app.route('/process', methods=['GET'])
 def process_data():
     """Fetches the data from Azure Storage and performs the analysis."""
     global df
-
-    # Connect to Azure Storage and download the data
     blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
-    download_stream = blob_client.download_blob().readall()
-
-    # Load the data into a DataFrame
-    df = pd.read_json(io.StringIO(download_stream.decode('utf-8')))
-
-    # Only keep the 'text' column
+    download_stream = blob_client.download_blob()
+    df = pd.read_json(io.BytesIO(download_stream.readall()))
     df = df[['text']]
-
-    # Create a new column for chunk IDs
-    df['Chunk'] = np.arange(len(df)) // 100
-
-    # Apply sentiment analysis to each chunk separately
-    for _, chunk_df in df.groupby('Chunk'):
-        chunk_df['Analysis'] = chunk_df['text'].apply(analyze_text)
-
-    # Drop the 'Chunk' column
-    df = df.drop(columns=['Chunk'])
-
-    # Save the DataFrame to a JSON file
+    df = analyze_text_in_chunks(df)
     df.to_json('Analysed_Tweets.json', orient='records')
-
     return jsonify({'message': 'Data processed successfully'}), 200
+
+@app.route('/summarize', methods=['GET'])
+def summarize_analysis():
+    """Summarizes the analysis results."""
+    global df
+    if df is None or 'Analysis' not in df.columns:
+        return jsonify({'error': 'No analysis data available'}), 400
+    summary = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a research assistant who has just completed an analysis of public sentiment towards the Panama Papers tax scandal using GPT-3.5 Turbo. Please create an academic summary of the results."},
+            {"role": "user", "content": ' '.join(df['Analysis'].dropna())}
+        ],
+        temperature=0.3,
+        max_tokens=4096
+    )
+    return jsonify({'summary': summary['choices'][0]['message']['content'].strip()}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
