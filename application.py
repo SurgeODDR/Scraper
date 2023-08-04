7a8cf19c12e0027360a567197f275e7aa0428ab2
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-08-04 14:08:31 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-import logging
 from flask import Flask, request, jsonify
 import pandas as pd
 import openai
@@ -6,74 +5,98 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
 import os
-import textwrap
-from docx import Document
 import io
-
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+import json
+import logging
+import time
 
 # Create a Flask instance
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 # Initialize a global variable for the DataFrame
 df = None
 
 # Authenticate using the managed identity
-logging.info("Authenticating with Azure...")
 credential = DefaultAzureCredential()
 
 # Create a SecretClient using the credential
-logging.info("Creating SecretClient...")
 key_vault_uri = "https://Keyvaultxscrapingoddr.vault.azure.net/"
 secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
 # Get the OpenAI API key from the Key Vault
-logging.info("Fetching OpenAI API key...")
 openai.api_key = secret_client.get_secret("openai-api-key").value
 
 def analyze_text(text):
-    """Performs sentiment analysis using GPT-3."""
-    logging.info("Analyzing text...")
+    """Performs topic modeling, sentiment analysis, and emotional tone analysis using GPT-3."""
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a research assistant analyzing public sentiment towards the Panama Papers tax scandal. Analyze the following text for key themes, sentiment, and emotional tone:"},
             {"role": "user", "content": text}
         ],
         temperature=0.3,
-        max_tokens=6000
+        max_tokens=200
     )
     return response['choices'][0]['message']['content'].strip()
 
 @app.route('/process', methods=['GET'])
 def process_data():
     """Fetches the data from Azure Storage and performs the analysis."""
-    global df
-    logging.info("Fetching data from Azure Storage...")
     blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
-    download_stream = blob_client.download_blob().content_as_text()
-    df = pd.read_json(io.StringIO(download_stream))
-    df = df[['text']]
-    logging.info("Performing sentiment analysis on data...")
-    df['Analysis'] = df['text'].apply(analyze_text)
-    df.to_json('Analysed_Tweets.json', orient='records')
+    download_stream = blob_client.download_blob()
+    df = pd.read_json(io.BytesIO(download_stream.readall()), lines=True)
+
+    chunk_size = 100  # Adjust this value based on your needs
+    num_chunks = len(df) // chunk_size
+    if len(df) % chunk_size:
+        num_chunks += 1
+
+    app.logger.info(f"Processing {num_chunks} chunks")
+
+    for i in range(num_chunks):
+        app.logger.info(f"Processing chunk {i + 1} of {num_chunks}")
+        try:
+            chunk_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", f"Analysed_Tweets_{i}.json")
+            if chunk_blob_client.exists():
+                app.logger.info(f"Chunk {i + 1} already processed, skipping")
+                continue
+
+            start = i * chunk_size
+            end = start + chunk_size
+            df_chunk = df[start:end]
+            df_chunk['Analysis'] = df_chunk['text'].apply(analyze_text)
+            chunk_json = df_chunk.to_json(orient='records')
+            chunk_blob_client.upload_blob(chunk_json, overwrite=True)
+
+            app.logger.info(f"Finished processing chunk {i + 1}")
+            time.sleep(5)  # Add a delay to prevent overloading the server
+        except Exception as e:
+            app.logger.error(f"Error processing chunk {i + 1}: {str(e)}")
+
     return jsonify({'message': 'Data processed successfully'}), 200
 
-def summarize_results(data):
-    """Summarizes the analysis results using GPT-3."""
-    logging.info("Summarizing analysis results...")
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=[
-            {"role": "system", "content": "You are a research assistant who has analyzed public sentiment towards the Panama Papers tax scandal using topic modeling, sentiment analysis, and emotional tone analysis. Your task is to create an academic summary of the findings."},
-            {"role": "user", "content": data}
-        ],
-        temperature=0.3,
-        max_tokens=12000
-    )
-    return response['choices'][0]['message']['content'].strip()
+@app.route('/consolidate', methods=['GET'])
+def consolidate_data():
+    """Fetches the chunked data from Azure Storage and consolidates it into a single file."""
+    blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
+    container_client = blob_service_client.get_container_client("scrapingstoragecontainer")
+
+    df_consolidated = pd.DataFrame()
+
+    for blob in container_client.list_blobs():
+        if "Analysed_Tweets_" in blob.name:
+            app.logger.info(f"Consolidating {blob.name}")
+            blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", blob.name)
+            download_stream = blob_client.download_blob()
+            df_chunk = pd.read_json(io.BytesIO(download_stream.readall()), lines=True)
+            df_consolidated = pd.concat([df_consolidated, df_chunk])
+
+    consolidated_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Analysed_Tweets.json")
+    consolidated_blob_client.upload_blob(df_consolidated.to_json(orient='records'), overwrite=True)
+
+    return jsonify({'message': 'Data consolidated successfully'}), 200
 
 @app.route('/summarize', methods=['GET'])
 def summarize_data():
