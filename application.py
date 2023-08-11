5903032f12e55bf602d3ba041b1dfd2e6e4f7f25
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-08-11 10:47:10 +0200

    Update application.py

--- application.py
+++ application.py
@@ -5,9 +5,7 @@ import openai
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
-import os
 import io
-import json
 import sys
 import logging
 import time
@@ -22,9 +20,10 @@ key_vault_uri = "https://Keyvaultxscrapingoddr.vault.azure.net/"
 secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 openai.api_key = secret_client.get_secret("openai-api-key").value
 
-rate_limiter = RateLimiter(max_calls=3500, period=60)  # 3,500 requests per minute
+rate_limiter = RateLimiter(max_calls=3500, period=60)
 
 def analyze_text(text):
+    print("Analyzing text...")
     headers = {"Authorization": f"Bearer {openai.api_key}"}
     data = {
         "model": "gpt-3.5-turbo-16k",
@@ -55,40 +54,34 @@ Structure the CSV output as follows:
         response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
         response_data = response.json()
 
-    if 'error' in response_data and response_data['error']['code'] == 'rate_limit_exceeded':
-        time.sleep(1)
-        return analyze_text(text)
-
     if 'choices' in response_data:
         return response_data['choices'][0]['message']['content'].strip()
     else:
-        app.logger.error(f"Unexpected response from OpenAI: {response_data}")
+        print("Error in OpenAI response.")
         return "Error analyzing the text."
 
-LAST_PROCESSED_ID_BLOB_NAME = "last_processed_id.txt"
-
 def get_last_processed_tweet_id(blob_service_client):
-    """Fetch the last processed tweet ID from Azure Blob Storage."""
-    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", LAST_PROCESSED_ID_BLOB_NAME)
+    print("Fetching last processed tweet ID...")
+    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "last_processed_id.txt")
     if blob_client.exists():
         download_stream = blob_client.download_blob()
         return int(download_stream.readall())
     return None
 
 def set_last_processed_tweet_id(blob_service_client, tweet_id):
-    """Save the last processed tweet ID to Azure Blob Storage."""
-    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", LAST_PROCESSED_ID_BLOB_NAME)
+    print(f"Setting last processed tweet ID: {tweet_id}...")
+    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "last_processed_id.txt")
     blob_client.upload_blob(str(tweet_id), overwrite=True)
 
 @app.route('/process', methods=['GET'])
 def process_data():
+    print("Processing data...")
     blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
     download_stream = blob_client.download_blob()
     data = download_stream.readall()
     df = pd.read_json(io.BytesIO(data))
 
-    # Fetch the last processed tweet ID and use it as a starting point
     last_processed_id = get_last_processed_tweet_id(blob_service_client)
     if last_processed_id:
         df = df[df['id'] > last_processed_id]
@@ -99,48 +92,28 @@ def process_data():
     for chunk_df in chunks:
         chunk_text = chunk_df['text'].str.cat(sep='\n')
         analysis = analyze_text(chunk_text)
-        
-        # Save the ID of the last tweet in the chunk to Azure Blob Storage
         last_tweet_id_in_chunk = chunk_df.iloc[-1]['id']
         set_last_processed_tweet_id(blob_service_client, last_tweet_id_in_chunk)
-
-        # Here's where you should update the aggregate analysis
         update_aggregate_analysis(analysis, len(chunk_df))
 
-    return jsonify({'message': 'Data processed and aggregate analysis updated successfully'}), 200
-
-        
-import time
+    return jsonify({'message': 'Data processed successfully'}), 200
 
 def update_aggregate_analysis(analysis, tweets_processed):
-    """Updates the aggregate_analysis.txt with the summary_chunk and uploads it to Azure Blob Storage."""
-
+    print("Updating aggregate analysis...")
     aggregate_path = "/tmp/aggregate_analysis.txt"
     blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
-    
-    # Default values
     aggregate_text = ""
     iteration = 1
-    
-    # Check if the aggregate_analysis.txt file exists in Blob Storage
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "aggregate_analysis.txt")
-    if blob_client.exists():
-        # Download the file from Blob Storage
-        download_stream = blob_client.download_blob()
-        with open(aggregate_path, 'wb') as file:
-            file.write(download_stream.readall())
-        with open(aggregate_path, 'r') as file:
-            aggregate_text = file.read()
-            # Extract the last iteration number from the aggregate text
-            last_line = aggregate_text.strip().split('\n')[-1]
-            if "Iteration" in last_line:
-                iteration = int(last_line.split(" ")[1].replace(":", "")) + 1
     
-    headers = {
-        "Authorization": f"Bearer {openai.api_key}"
-    }
-
-    # Modify the prompt to include the number of tweets processed
+    if blob_client.exists():
+        download_stream = blob_client.download_blob()
+        aggregate_text = download_stream.readall().decode('utf-8')
+        last_line = aggregate_text.strip().split('\n')[-1]
+        if "Iteration" in last_line:
+            iteration = int(last_line.split(" ")[1].replace(":", "")) + 1
+    
+    headers = {"Authorization": f"Bearer {openai.api_key}"}
     data = {
         "model": "gpt-3.5-turbo-16k",
         "messages": [
@@ -169,79 +142,50 @@ def update_aggregate_analysis(analysis, tweets_processed):
     response_data = response.json()
 
     if 'choices' in response_data:
-        updated_text = response_data['choices'][0]['message']['content'].strip()
-        
-        # Append the iteration number and timestamp to the updated text
-        updated_text += f"\n\n---\nIteration: {iteration} | Updated on: {time.strftime('%Y-%m-%d %H:%M:%S')}"
-
-        # Write the updated text to the aggregate_analysis.txt
+        updated_text = response_data['choices'][0]['message']['content'].strip() + f"\n\n---\nIteration: {iteration} | Updated on: {time.strftime('%Y-%m-%d %H:%M:%S')}"
         with open(aggregate_path, 'w') as file:
             file.write(updated_text)
-
-        # Upload the updated aggregate_analysis.txt to Azure Blob Storage
         with open(aggregate_path, 'rb') as data:
             blob_client.upload_blob(data, overwrite=True)
-        logging.info(f"Uploaded {aggregate_path} to Azure Blob Storage successfully")
 
-    else:
-        error_message = f"Unexpected response from OpenAI: {response_data}"
-        app.logger.error(error_message)
-        logging.error(error_message)
-       
-
-FLAG_TRIGGER_PROCESS = True  # Set this flag to True to trigger /process
+FLAG_TRIGGER_PROCESS = True
 
 def monitor_and_trigger_process():
-    global FLAG_TRIGGER_PROCESS
     while True:
         if FLAG_TRIGGER_PROCESS:
             try:
-                # Send a request to the /process endpoint
-                response = requests.get("https://gptanalyser.azurewebsites.net/process")
-                if response.status_code == 200:
-                    FLAG_TRIGGER_PROCESS = False  # Reset the flag after successful processing
+                requests.get("https://gptanalyser.azurewebsites.net/process")
+                FLAG_TRIGGER_PROCESS = False
             except Exception as e:
-                app.logger.error(f"Failed to trigger /process: {e}")
-            time.sleep(60)  # Check every minute
+                print(f"Error: {e}")
+            time.sleep(60)
 
 def run_app():
     app.run(debug=True)
 
 if __name__ == "__main__":
+    from multiprocessing import Process
     process = Process(target=run_app)
     process.start()
-
-    # Start the monitoring process
     monitor_process = Process(target=monitor_and_trigger_process)
     monitor_process.start()
 
     while True:
         try:
-            # Check every minute if the Flask process is still alive
             process.join(timeout=60)
             if not process.is_alive():
                 break
         except (KeyboardInterrupt, SystemExit):
-            # Gracefully shutdown
             process.terminate()
             monitor_process.terminate()
             process.join()
             monitor_process.join()
             break
         except Exception as e:
-            # Log the error for debugging purposes
-            app.logger.error(f"Exception encountered: {e}")
-
-            # Try to gracefully terminate the processes
+            print(f"Exception: {e}")
             process.terminate()
             monitor_process.terminate()
-            process.join()
-            monitor_process.join()
-
-            # Restart the Flask app in a new process
             process = Process(target=run_app)
             process.start()
-
-            # Restart the monitoring process
             monitor_process = Process(target=monitor_and_trigger_process)
             monitor_process.start()
