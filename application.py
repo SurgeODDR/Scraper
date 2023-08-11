2cdcac05d694d2bf312504fe5eebb3e77b088f86
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-08-11 09:16:24 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
-import signal
-import sys
-from multiprocessing import Process
-from flask import Flask, request, jsonify
+import requests
+from flask import Flask, jsonify
 import pandas as pd
 import openai
 from azure.identity import DefaultAzureCredential
@@ -10,38 +8,24 @@ from azure.storage.blob import BlobServiceClient
 import os
 import io
 import json
+import sys
 import logging
 import time
-from docx import Document
-from azure.core.exceptions import AzureError
-import requests
+from ratelimiter import RateLimiter
 
-# Create a Flask instance
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
-
-# Configure logging for Azure Log Stream
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-# Initialize a global variable for the DataFrame
-df = None
-
-# Authenticate using the managed identity
 credential = DefaultAzureCredential()
-
-# Create a SecretClient using the credential
 key_vault_uri = "https://Keyvaultxscrapingoddr.vault.azure.net/"
 secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
-
-# Get the OpenAI API key from the Key Vault
 openai.api_key = secret_client.get_secret("openai-api-key").value
 
+rate_limiter = RateLimiter(max_calls=3500, period=60)  # 3,500 requests per minute
+
 def analyze_text(text):
-    start_time = time.time()
-    headers = {
-        "Authorization": f"Bearer {openai.api_key}"
-    }
-    
+    headers = {"Authorization": f"Bearer {openai.api_key}"}
     data = {
         "model": "gpt-3.5-turbo-16k",
         "messages": [
@@ -70,11 +54,15 @@ Structure the CSV output as follows:
         "max_tokens": 12000
     }
     
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-    response_data = response.json()
+    with rate_limiter:
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
+        response_data = response.json()
+
+    if 'error' in response_data and response_data['error']['code'] == 'rate_limit_exceeded':
+        time.sleep(1)
+        return analyze_text(text)
 
     if 'choices' in response_data:
-        app.logger.info(f"Analysis took {time.time() - start_time} seconds")
         return response_data['choices'][0]['message']['content'].strip()
     else:
         app.logger.error(f"Unexpected response from OpenAI: {response_data}")
@@ -82,86 +70,25 @@ Structure the CSV output as follows:
 
 @app.route('/process', methods=['GET'])
 def process_data():
-    try:
-        blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
+    blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
+    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
+    download_stream = blob_client.download_blob()
+    data = download_stream.readall()
+    df = pd.read_json(io.BytesIO(data))
+    chunk_size = 10
+    chunks = [df['text'][i:i+chunk_size].str.cat(sep='\n') for i in range(0, len(df), chunk_size)]
+    tweets_processed = 0
+
+    for i, chunk in enumerate(chunks):
+        summary_blob_name = f"Summary_Chunk_{i}.txt"
+        summary_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", summary_blob_name)
         
-        # Fetch the data from Tweets.json
-        blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
-        download_stream = blob_client.download_blob()
-        data = download_stream.readall()
-        app.logger.info(f"Data from blob: {data[:100]}")
-        df = pd.read_json(io.BytesIO(data))
-        
-        # Split the data into chunks for analysis
-        chunk_size = 10  # Adjust as necessary
-        chunks = [df['text'][i:i+chunk_size].str.cat(sep='\n') for i in range(0, len(df), chunk_size)]
-        
-        # Initialize the tweets_processed variable
-        tweets_processed = 0
-        
-        # Analyze each chunk and update aggregate analysis
-        for i, chunk in enumerate(chunks):
-            # Analyze the chunk
+        if not summary_blob_client.exists():
             analysis = analyze_text(chunk)
-            tweets_processed += len(chunk.split('\n'))  # Count the number of tweets in the current chunk
-            
-            # Update the aggregate analysis with this analysis and the number of tweets processed
+            tweets_processed += len(chunk.split('\n'))
             update_aggregate_analysis(analysis, tweets_processed)
-        
-        return jsonify({'message': 'Data processed and aggregate analysis updated successfully'}), 200
 
-    except AzureError as ae:
-        app.logger.error(f"AzureError encountered: {str(ae)}")
-    except Exception as e:
-        app.logger.error(f"General error encountered: {str(e)}")
-    
-    return jsonify({'error': 'An error occurred while processing the data.'}), 500
-        
-def summarize_chunk(chunk_text):
-    """Summarizes a chunk of text using OpenAI."""
-    headers = {
-        "Authorization": f"Bearer {openai.api_key}"
-    }
-    
-    tweet_count = len(chunk_text.split('\n'))
-    
-    data = {
-        "model": "gpt-3.5-turbo-16k",
-        "messages": [
-            {
-                "role": "system",
-                "content": f""""
-You are analyzing a set of {tweet_count} tweets. Provide a quantitative summary in CSV format. The summary should contain:
-- How many Tweets are in this dataset
-- Distribution of sentiments (Positive, Negative, Neutral) with percentages.
-- Distribution of key emotions (Anger, Distrust, Skepticism, Outrage/Indignation) with percentages.
-- Total mentions of keywords related to inequality and corruption, and their associated sentiment percentages.
-
-Structure the CSV output as follows:
-"Category, Positive (%), Negative (%), Neutral (%), Total Mentions"
-"Sentiments, x%, y%, z%, Total"
-"Emotions: Anger, a%, -, -, Total"
-"Emotions: Distrust, b%, -, -, Total"
-...
-"Keywords: Inequality, -, p%, -, Total"
-"Keywords: Corruption, -, q%, -, Total"
-...
-"""
-            },
-            {"role": "user", "content": chunk_text}
-        ],
-        "temperature": 0.3,
-        "max_tokens": 12000
-    }
-    
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-    response_data = response.json()
-    
-    if 'choices' in response_data:
-        return response_data['choices'][0]['message']['content'].strip()
-    else:
-        app.logger.error(f"Unexpected response from OpenAI: {response_data}")
-        return "Error summarizing the data."
+    return jsonify({'message': 'Data processed and aggregate analysis updated successfully'}), 200
         
 import time
 
@@ -228,63 +155,6 @@ def update_aggregate_analysis(analysis, tweets_processed):
         app.logger.error(error_message)
         logging.error(error_message)
        
-
-@app.route('/summarize', methods=['GET'])
-def summarize_data():
-    try:
-        # Initialize the BlobServiceClient
-        blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
-        
-        # Get the blob client for the consolidated JSON file
-        blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Analysed_Tweets.json")
-
-        # Download the JSON file
-        download_stream = blob_client.download_blob()
-        df = pd.read_json(io.BytesIO(download_stream.readall()))
-
-        # Split the data into chunks for summarization
-        chunk_size = 10
-        chunks = [df['Analysis'][i:i+chunk_size].str.cat(sep='\n') for i in range(0, len(df), chunk_size)]
-
-        # Summarize each chunk and update aggregate analysis
-        tweets_processed = 0
-        for i, chunk in enumerate(chunks):
-            summary_blob_name = f"Summary_Chunk_{i}.txt"
-            summary_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", summary_blob_name)
-            
-            if not summary_blob_client.exists():
-                # Summarize the chunk
-                summary = summarize_chunk(chunk)
-                tweets_processed += len(chunk.split('\n'))  # Count the number of tweets in the current chunk
-                
-                # Store the summary
-                summary_blob_client.upload_blob(summary, overwrite=True)
-                # Update the aggregate analysis with this summary and the number of tweets processed
-                update_aggregate_analysis(summary, tweets_processed)
-        
-        # After all chunks are processed, fetch the final aggregate analysis
-        with open("/tmp/aggregate_analysis.txt", 'r') as file:
-            final_summary = file.read()
-
-        # Create a Document instance and add the final summary to it
-        doc = Document()
-        doc.add_paragraph(final_summary)
-
-        # Save the document to a .docx file
-        doc_path = '/tmp/Final_Summary.docx'
-        doc.save(doc_path)
-
-        # Upload the .docx file to Azure Blob Storage
-        blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Final_Summary.docx")
-        with open(doc_path, 'rb') as data:
-            blob_client.upload_blob(data, overwrite=True)
-
-        return jsonify({'message': 'Final summary created and saved successfully'}), 200
-
-    except Exception as e:
-        app.logger.error(f"Error in /summarize route: {e}")
-        return jsonify({'error': 'An error occurred while summarizing the data.'}), 500
-
 
 FLAG_TRIGGER_PROCESS = True  # Set this flag to True to trigger /process
 
