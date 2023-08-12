9b0f189d6427356c2f140a053f6c16e5017e0f74
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-08-12 21:14:20 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import requests
+from flask import Flask, jsonify
 import pandas as pd
 import openai
-from flask import Flask, jsonify
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
-from ratelimiter import RateLimiter
 import io
 import logging
+from ratelimiter import RateLimiter
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -17,131 +17,124 @@ key_vault_uri = "https://Keyvaultxscrapingoddr.vault.azure.net/"
 secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
 openai.api_key = secret_client.get_secret("openai-api-key").value
 
-# Rate limiter for OpenAI API
+# Rate limiter for the first OpenAI API key
 rate_limiter = RateLimiter(max_calls=3500, period=60)
 
-def openai_request(data):
-    headers = {"Authorization": f"Bearer {openai.api_key}"}
-    for _ in range(3):  # 3 retries
-        with rate_limiter:
+import time
+
+def openai_request(data, api_key, rate_limiter_obj, retries=3, delay=5):
+    headers = {"Authorization": f"Bearer {api_key}"}
+    
+    for _ in range(retries):
+        with rate_limiter_obj:
             response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
             if response.status_code == 429:  # Rate Limit Exceeded
-                app.logger.warning("Rate limit exceeded. Retrying...")
+                app.logger.warning("Rate limit exceeded. Retrying in {} seconds...".format(delay))
+                time.sleep(delay)
                 continue
             response.raise_for_status()
             return response.json()
-    app.logger.error("Failed to get a successful response from OpenAI.")
-    return {}
+    app.logger.error("Failed to make a successful request after {} retries.".format(retries))
+    return {}  # Return an empty dictionary to indicate failure
+
 
 def save_to_blob(blob_service_client, content, file_name):
+    path = f"/tmp/{file_name}"
+    with open(path, 'w') as file:
+        file.write(content)
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", file_name)
-    blob_client.upload_blob(content, overwrite=True)
+    with open(path, 'rb') as data:
+        blob_client.upload_blob(data, overwrite=True)
 
 def get_processed_tweet_ids(blob_service_client):
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "processed_tweet_ids.txt")
     if blob_client.exists():
-        processed_ids = set(map(int, blob_client.download_blob().readall().decode('utf-8').splitlines()))
+        download_stream = blob_client.download_blob()
+        processed_ids = set(map(int, download_stream.readall().decode('utf-8').splitlines()))
         return processed_ids
     return set()
+
+def update_processed_tweet_ids(blob_service_client, processed_ids):
+    blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "processed_tweet_ids.txt")
+    ids_str = "\n".join(map(str, processed_ids))
+    blob_client.upload_blob(ids_str, overwrite=True)
 
 def analyze_text(text):
     data = {
         "model": "gpt-3.5-turbo-16k",
         "messages": [
-            {
-                "role": "system",
-                "content": """
-                    Analyze the provided text and extract mentions of politicians and celebrities. For each figure mentioned, categorize the sentiments, emotions, and specific keywords associated with them. Ensure the output is strictly in CSV format with the following structure:
-
-                    "Name, Sentiment/Emotion/Keyword, Count"
-
-                    For clarity:
-                    If John Doe is mentioned with positive sentiment 5 times, the output should include:
-                    "John Doe, Positive Sentiment, 5"
-                    
-                    Similarly, if Jane Smith is associated with happiness 3 times:
-                    "Jane Smith, Happiness, 3"
-
-                    The output should strictly adhere to the CSV format mentioned above without any additional text.
-                """
+            {"role": "system", "content": """
+        Generate a quantitative analysis in CSV format based on the provided text. Cover:
+        - Sentiments regarding politicians and celebrities (Positive, Negative, Neutral)
+        - Key emotions regarding politicians and celebrities (happiness, sadness, anger, fear, surprise, disgust, jealousy, outrage/indignation, distrust/skepticism, despair/hopelessness, shock/astonishment, relief, and empowerment)
+        - Keywords regarding politicians and celebrities related to inequality, unfairness, distrust in government, unjust actions, disloyalty, and perceptions of corruption.
+        
+        CSV Structure:
+        "Name, Category, Total Mentions"
+        "[Politician/Celebrity Name], Sentiments: Positive, [Total Positive Sentiment Mentions for this individual]"
+        "[Politician/Celebrity Name], Sentiments: Negative, [Total Negative Sentiment Mentions for this individual]"
+        "[Politician/Celebrity Name], Sentiments: Neutral, [Total Neutral Sentiment Mentions for this individual]"
+        "[Politician/Celebrity Name], Emotions: Happiness, [Total Happiness Mentions for this individual]"
+        ...
+        "[Politician/Celebrity Name], Keywords: Inequality, [Total Inequality Mentions for this individual]"""
+        ...
             },
             {"role": "user", "content": text}
         ],
-        "temperature": 0.3,
+        "temperature": 0.1,
         "max_tokens": 13000
     }
-
-    response_data = openai_request(data)  # Using the function without unnecessary arguments
-    if 'choices' in response_data:
-        return response_data['choices'][0]['message']['content'].strip()
-    return "Error analyzing the text."
-
-import re
-
-def flexible_column_matching(df, keyword):
-    """Find the column name that best matches the keyword."""
-    for col in df.columns:
-        if keyword.lower() in col.lower():
-            return col
-    return None
+    
+    response_data = openai_request(data, openai.api_key, rate_limiter)
+    return response_data['choices'][0]['message']['content'].strip() if 'choices' in response_data else "Error analyzing the text."
 
 def combine_and_save_analysis(blob_service_client, new_analysis):
-    try:
-        # Read the new analysis, ensuring that any NaN values are filled with the string 'NaN'
-        new_df = pd.read_csv(io.StringIO(new_analysis), index_col=0, dtype=str).fillna('NaN')
-        
-        # Standardize the index labels (remove counts)
-        new_df.index = new_df.index.str.split(',').str[0]
+    new_df = pd.read_csv(io.StringIO(new_analysis), index_col=0)
 
-        celeb_db_analysis_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "celeb_db_analysis.csv")
-        
-        if celeb_db_analysis_blob_client.exists():
-            existing_content = celeb_db_analysis_blob_client.download_blob().readall().decode('utf-8')
-            
-            # Read the existing content, ensuring that any NaN values are filled with the string 'NaN'
-            existing_df = pd.read_csv(io.StringIO(existing_content), index_col=0, dtype=str).fillna('NaN')
-            
-            # Standardize the index labels of the existing df
-            existing_df.index = existing_df.index.str.split(',').str[0]
-            
-            # Convert values to numeric for addition. Fill NaN with 0 for addition.
-            combined_df = new_df.apply(pd.to_numeric, errors='coerce').add(existing_df.apply(pd.to_numeric, errors='coerce'), fill_value=0)
-        else:
-            combined_df = new_df
+    # Standardize the index labels (remove counts)
+    new_df.index = new_df.index.str.split(',').str[0]
 
-        combined_csv_content = combined_df.to_csv()
-        save_to_blob(blob_service_client, combined_csv_content, "celeb_db_analysis.csv")
-    except Exception as e:
-        app.logger.error(f"Error in combine_and_save_analysis: {str(e)}")
+    db_analysis_blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "db_analysis.csv")
+    if db_analysis_blob_client.exists():
+        existing_content = db_analysis_blob_client.download_blob().readall().decode('utf-8')
+        existing_df = pd.read_csv(io.StringIO(existing_content), index_col=0)
+        
+        # Standardize the index labels of the existing df
+        existing_df.index = existing_df.index.str.split(',').str[0]
+
+        combined_df = new_df.add(existing_df, fill_value=0)
+    else:
+        combined_df = new_df
+
+    combined_csv_content = combined_df.to_csv()
+    save_to_blob(blob_service_client, combined_csv_content, "c_db_analysis.csv")
 
 @app.route('/process', methods=['GET'])
 def process_data():
     blob_service_client = BlobServiceClient(account_url="https://scrapingstoragex.blob.core.windows.net", credential=credential)
-    
-    # Fetch the tweets from Azure Blob Storage
     blob_client = blob_service_client.get_blob_client("scrapingstoragecontainer", "Tweets.json")
     download_stream = blob_client.download_blob()
     data = download_stream.readall()
     df = pd.read_json(io.BytesIO(data))
     
-    # Retrieve the list of tweet IDs that have been previously processed
+    # Get the list of tweet IDs that have been processed already
     processed_ids = get_processed_tweet_ids(blob_service_client)
 
-    # Filter out tweets that have been processed before
+    # Filter out tweets that have been processed
     df = df[~df['id'].isin(processed_ids)]
     
-    # Split the remaining tweets into chunks for processing
-    chunk_size = 5
+    # Splitting data into chunks
+    chunk_size = 5  # Adjust as needed
     chunks = [df.iloc[i:i + chunk_size] for i in range(0, len(df), chunk_size)]
     
-    # Process each chunk and update the aggregate analysis CSV file
+    # Processing each chunk and updating db_analysis.csv
     for chunk_df in chunks:
-        chunk_text = chunk_df['text'].astype(str).str.cat(sep='\n')
+        chunk_text = chunk_df['text'].str.cat(sep='\n')
         new_analysis = analyze_text(chunk_text)
         combine_and_save_analysis(blob_service_client, new_analysis)
 
-        # Update the list of processed tweet IDs and save back to Azure Blob Storage
+        # Update the list of processed tweets
         processed_ids.update(chunk_df['id'].tolist())
-        save_to_blob(blob_service_client, "\n".join(map(str, processed_ids)), "processed_tweet_ids.txt")
+        update_processed_tweet_ids(blob_service_client, processed_ids)
 
     return jsonify({'message': 'Data processed successfully'}), 200
